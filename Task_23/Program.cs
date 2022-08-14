c6f57cd710e7dc7c20c1643ee8966a02b320cf05
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-15 00:12:05 +0400

    Task_23 added

---
+++ Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter some number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+userNumber = Math.Abs(userNumber);
+int consoleNum = 1;
+
+while (consoleNum <= userNumber)
+{
+    Console.Write($" {consoleNum} cubed is: ");
+    Console.WriteLine (Math.Pow(consoleNum, 3));
+    consoleNum++;
+}
