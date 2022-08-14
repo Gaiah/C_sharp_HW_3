c319b93e8fd10e3fbd28d0e2d01e060af8405b91
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-15 00:01:16 +0400

    Task_19 added

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter a five-digit number: ");
+decimal num = Convert.ToDecimal(Console.ReadLine());
+num = Math.Abs(num);
+
+if (num > 9999 && num < 100000) 
+{
+    if (Math.Round(num / 10000, MidpointRounding.ToZero) == Math.Round(num % 10, MidpointRounding.ToZero))
+    {
+        if (Math.Round(num / 1000 % 10, MidpointRounding.ToZero) == Math.Round(num / 10 % 10, MidpointRounding.ToZero))
+        {
+            Console.WriteLine($"This is a palindromic number: {num}");
+        }
+        else
+        {
+            Console.WriteLine($"This isn't a palindromic number: {num} second couple failed");
+        }
+    }
+    else 
+    {
+        Console.WriteLine($"This isn't a palindromic number: {num} first couple failed");
+    }
+}
+else
+{
+    Console.WriteLine($"This is not a five-digit number: {num}");
+}
